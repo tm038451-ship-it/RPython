a48070ca8330f17e99f27be21cc9acbaad72f65d
Tati <ya.tatimur@yandex.ru>
2026-06-28 14:14:46 +0400

    Добавлены уроки 1-3

---
+++ 03_lesson/user.py
@@ -0,0 +1,23 @@
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def print_first_name(self):
+        print(self.first_name)
+
+    def print_last_name(self):
+        print(self.last_name)
+
+    def print_full_name(self):
+        print(f"{self.first_name} {self.last_name}")
+
+from user import User
+
+my_user = User("Иван", "Иванов")
+
+
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
+        
