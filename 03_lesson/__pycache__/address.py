8308b443f3eae173116ad934aa6d399e7abcac6a
tm038451-ship-it <286664214+tm038451-ship-it@users.noreply.github.com>
2026-06-29 15:24:26 +0400

    Delete 03_lesson directory

--- 03_lesson/__pycache__/address.py
+++
@@ -1,8 +0,0 @@
-class Address:
-    def __init__(self, index: str, city: str, street: str, house: str, apartment: str):
-        self.index = index
-        self.city = city
-        self.street = street
-        self.house = house
-        self.apartment = apartment
-        
