0c2bbd7b10190afe8f9e445361cb16c7d263efb7
tm038451-ship-it <286664214+tm038451-ship-it@users.noreply.github.com>
2026-06-29 15:22:48 +0400

    Delete lesson_01 directory

--- lesson_01/02_lesson/lesson_2_task_7.py
+++
@@ -1,2 +0,0 @@
-my_list = list(range(18, 1, -4))
-print(my_list)
--- lesson_01/lesson_1_task_5.py
+++
@@ -1,6 +0,0 @@
-def print_number(num):
-    print(num, end='')
-
-digits = "88005553535"
-for digit in digits:
-    print_number(digit)
